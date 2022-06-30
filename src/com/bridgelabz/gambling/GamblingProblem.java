f109c4647871a2e72ca4b786be272064b2fb7784
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-30 19:52:05 +0530

    Added:code of Creating variables

---
+++ src/com/bridgelabz/gambling/GamblingProblem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.gambling;
+
+public class GamblingProblem {
+    final int StakePerGame = 1;
+    public static void main(String[] args) {
+        int perDayStake = 100;
+    }
+}
