4973e140c5f812e1946e38f677714996df37ebf5
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-30 22:25:08 +0530

    Added:code of Check gamer is win or loose

--- src/com/bridgelabz/gambling/GamblingProblem.java
+++ src/com/bridgelabz/gambling/GamblingProblem.java
@@ -2,8 +2,22 @@ package com.bridgelabz.gambling;
 
 public class GamblingProblem {
     final int StakePerGame = 1;
-
+    static int totalStake=0;
     public static void main(String[] args) {
         int perDayStake = 100;
+        GamblingProblem gamblingProblem = new GamblingProblem();
+        gamblingProblem.isWinOrLoose(gamblingProblem.StakePerGame,perDayStake);
+    }
+    public void isWinOrLoose(int StakePerGame,int perDayStake){
+        int bet = (int)(Math.random() * 2);
+        if (bet == 1){
+            System.out.println("Win 1$");
+            totalStake = StakePerGame + perDayStake;
+        }
+        else {
+            System.out.println("Loose 1$");
+            totalStake = perDayStake - StakePerGame;
+        }
+        System.out.println("Total Stake is "+totalStake);
     }
 }
