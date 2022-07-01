cc57ab09403ffcd6d12fcd8206514ccc9bae7ce5
surajrajegore <rajegoresuraj2880@gmail.com>
2022-07-01 07:08:55 +0530

    Added:code if stake is 50% then Exit from loop

--- src/com/bridgelabz/gambling/GamblingProblem.java
+++ src/com/bridgelabz/gambling/GamblingProblem.java
@@ -1,23 +1,41 @@
 package com.bridgelabz.gambling;
 
 public class GamblingProblem {
-    final int StakePerGame = 1;
-    static int totalStake=0;
     public static void main(String[] args) {
         int perDayStake = 100;
+        final int StakePerGame = 1;
+
         GamblingProblem gamblingProblem = new GamblingProblem();
-        gamblingProblem.isWinOrLoose(gamblingProblem.StakePerGame,perDayStake);
+        gamblingProblem.playGame(StakePerGame);
     }
-    public void isWinOrLoose(int StakePerGame,int perDayStake){
-        int bet = (int)(Math.random() * 2);
-        if (bet == 1){
+
+    public boolean isWinOrLoose() {
+        int bet = (int) (Math.random() * 2);
+        if (bet == 1) {
             System.out.println("Win 1$");
-            totalStake = StakePerGame + perDayStake;
-        }
-        else {
+            return true;
+
+        } else {
             System.out.println("Loose 1$");
-            totalStake = perDayStake - StakePerGame;
+            return false;
         }
-        System.out.println("Total Stake is "+totalStake);
+    }
+
+    public void playGame(int perDayStake) {
+        do {
+            int win = 0;
+            int loose = 0;
+            if (isWinOrLoose()) {
+                win += 1;
+                perDayStake += win;
+                System.out.println("totoalStake" + perDayStake);
+            } else {
+                loose += -1;
+                perDayStake += loose;
+                System.out.println("totalStake" + perDayStake);
+            }
+
+        } while (perDayStake < 50 || perDayStake >= 150);
+
     }
 }
